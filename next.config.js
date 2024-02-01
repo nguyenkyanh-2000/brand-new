1da4bf2bc7dba5d827ad31e1ceaa122308554dc0
Anh Ky Nguyen <anh163@gmail.com>
2024-02-01 13:49:27 +0700

    Test CORS

--- next.config.js
+++ next.config.js
@@ -4,16 +4,13 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/(.*)",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: process.env.NEXT_PUBLIC_LOCATION_ORIGIN,
-          }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
