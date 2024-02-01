15b1fb8a8d557ba4cf5adf2fef31b6c4048c45cc
Anh Ky Nguyen <anh163@gmail.com>
2024-02-01 14:06:56 +0700

    cors

--- app/api/route.js
+++ app/api/route.js
@@ -1,5 +1,18 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"; // defaults to auto
+
+export async function GET(request) {
+  return new Response("Hello, Next.js!", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
+
 export async function OPTIONS(request) {
   const allowedOrigin = request.headers.get("origin");
   const response = new NextResponse(null, {
